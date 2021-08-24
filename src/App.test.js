e0c1325adac0edb6a1806b8eaa8aa4ae37975d84
devarrowsmith <devarrowsmith@gmail.com>
2021-08-24 21:52:31 +0100

    Create react app. Remove superfluous content. Update directory structure.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './components/App';
 
-test('renders learn react link', () => {
+test('renders Estate Agent heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText("Estate Agent");
+  expect(headerElement).toBeInTheDocument();
 });
