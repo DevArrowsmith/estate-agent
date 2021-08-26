b45b90bfb5342b1b456ef2377d01622d01360872
devarrowsmith <devarrowsmith@gmail.com>
2021-08-26 21:27:52 +0100

    Configure linter. Update styles.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './components/App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./components/App";
 
-test('renders Estate Agent heading', () => {
+test("renders Estate Agent heading", () => {
   render(<App />);
   const headerElement = screen.getByText("Estate Agent");
   expect(headerElement).toBeInTheDocument();
